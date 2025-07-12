efae998033ca91ebdbf78ed58c0d664fea4c69ec
mohsen <amirimohsen72@gmail.com>
2025-07-12 21:24:09 +0330

    #3 message handler
    @bot.message_handler()
    send_message in massega_handler

--- core/main.py
+++ core/main.py
@@ -5,6 +5,10 @@ import os
 API_TOKEN = os.environ.get("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
-
+# @bot.message_handler(['command1','command2'])
+@bot.message_handler()
+def send_wellcome(message):
+    if message.text == 'start' :
+        bot.send_message(message.chat.id,'خوش آمدید')
 
 bot.infinity_polling()
