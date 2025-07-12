e8fb311b89fd883d13349a45aada6f299a6440ca
mohsen <amirimohsen72@gmail.com>
2025-07-12 21:31:14 +0330

    #4 reply_to & send_document
    bot.reply_to(message, 'پیام دریافت شد ')
    
    bot.send_document(message.chat.id,file)

--- core/main.py
+++ core/main.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 
+dir = os.path.dirname(os.path.abspath(__file__))
 
 API_TOKEN = os.environ.get("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
@@ -10,5 +11,14 @@ bot = telebot.TeleBot(API_TOKEN)
 def send_wellcome(message):
     if message.text == 'start' :
         bot.send_message(message.chat.id,'خوش آمدید')
+
+
+    elif message.text.startswith('file')  :
+        with open('core/testfile.txt','rb') as file:
+            bot.send_document(message.chat.id,file)
+
+    else :
+        bot.reply_to(message, 'پیام دریافت شد . بعد از بررسی پاسخ خواهیم داد')
+
 
 bot.infinity_polling()
